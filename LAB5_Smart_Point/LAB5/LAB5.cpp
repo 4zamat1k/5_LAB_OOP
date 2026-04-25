75a38aca9a18fb2495a920e3f62af05df5742979
4zamat1k <edvardhide25@gmail.com>
2026-04-25 08:59:52 +0500

    Smart point

---
+++ LAB5_Smart_Point/LAB5/LAB5.cpp
@@ -0,0 +1,121 @@
+﻿#include <iostream>
+#include <string>
+#include "memory"
+
+class Plane {
+protected:
+    std::string name;
+private:
+    double vehicleContent;
+public:
+    Plane(std::string name, double vehicleContent)
+    {
+        this->name = name;
+        this->vehicleContent = vehicleContent;
+        std::cout << "Plane " << name << std::endl;
+    }
+
+    Plane() {
+        name = "Unknown";
+        vehicleContent = 0.0;
+        std::cout << "Default Plane created" << std::endl;
+    }
+
+    Plane(const Plane& other) {
+        name = other.name;
+        vehicleContent = other.vehicleContent;
+        std::cout << "Plane " << name << " copied" << std::endl;
+    }
+
+    // Метод для вывода информации о самолете.
+    void print() {
+        std::cout << "Plane" << name
+            << ", Speed: " << vehicleContent << "Mach" << std::endl;
+    }
+
+    std::string getName() {
+        return name;
+    }
+    double getSpeed() {
+        return vehicleContent;
+    }
+
+    void setName(std::string newName) {
+        name = newName;
+    }
+
+    void setSpeed(double newSpeed) {
+        vehicleContent = newSpeed;
+    }
+
+    ~Plane() {
+        std::cout << "Plane" << name << " destroyed" << std::endl;
+    }
+
+
+};
+
+class Manufacturer : public Plane {
+private:
+    std::string country;
+public:
+    // Создан без параметров. 
+    Manufacturer() : Plane() {
+        country = "Unknown";
+        std::cout << "Default Manufacturer created" << std::endl;
+    }
+
+    // Создан с параметрами.
+    Manufacturer(std::string name, double vehicleContent, std::string country) : Plane(name, vehicleContent)
+    {
+        this->country = country;
+        std::cout << "Manufacturer " << name << " from " << country << std::endl;
+    }
+
+    // Конструктор копирования.
+    Manufacturer(const Manufacturer& other) : Plane(other) {
+        country = other.country;
+        std::cout << "Manufacturer " << getName() << " copied" << std::endl;
+    }
+
+    // Диструктор.
+    ~Manufacturer() {
+        std::cout << "Manufacturer " << country << " destroyed" << std::endl;;
+    }
+
+    void printManufacturer() {
+        std::cout << "Manufacturer: " << getName()
+            << ", Speed: " << getSpeed() << "Mach"
+            << ", Country: " << country << std::endl;
+    }
+    void testAccess() {
+        std::cout << name << std::endl;
+        std::cout << getSpeed() << std::endl;
+    }
+
+    // Переопределение метода print() для класса Manufacturer.
+    void print() {
+        Plane::print();
+        std::cout << "Country: " << country << std::endl;
+    }
+};
+
+int main() {
+    std::cout << "---Unique pointer---" << std::endl;
+    std::unique_ptr<Plane> p1 = std::make_unique<Plane>("Su-25", 1.5 );
+    p1->print();
+    std::cout << " " << std::endl;
+
+    std::cout << "---Unique pointer move---" << std::endl;
+    std::unique_ptr<Plane> p3 = std::make_unique<Plane>("F-16", 2.0 );
+    std::unique_ptr<Plane> p2 = std::move(p3);
+    p2->print();
+    std::cout << " " << std::endl;
+
+    std::cout << "---Shared pointer---" << std::endl;
+    std::shared_ptr<Plane> s1 = std::make_shared<Plane>("Z-5", 1.8 );
+    std::shared_ptr<Plane> s2 = s1;
+    s1->print();
+    s2->print();
+
+}
